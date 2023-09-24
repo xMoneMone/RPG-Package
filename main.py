f78f67ef8cf15d8b6221f1c2561c2b8057ea2245
Ellie <yokomone@yahoo.com>
2023-09-24 18:21:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     height = monitor.current_h
 
     game_screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption("Void Game")
+    pygame.display.set_caption("Game")
 
     clock = pygame.time.Clock()
 
